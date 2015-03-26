be99de31324e38d624b78aa1777e3be4e2afef05
irfanbaqui <irfan.baqui@gmail.com>
2015-03-26 15:38:43 -0700

    Implement all methods in the Set class and pass all tests.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .add() is a constant time function.
+ * .contains() is a constant time function.
+ * .remove() is a constant time function.
  */
