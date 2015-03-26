a1462f52d982d981d53878bb3c23bf37b7a7453e
irfanbaqui <irfan.baqui@gmail.com>
2015-03-26 09:57:28 -0700

    Implement the correct instantiation pattern for stack and queue.

--- sprint-one/src/pseudoclassical/extraCredit.js
+++ sprint-one/src/pseudoclassical/extraCredit.js
@@ -1,6 +1,6 @@
 var stackList = [];
 var queueList = [];
-for (var i = 0; i < 10000; i++) {
-  stackList.push(Stack());
-  queueList.push(Queue());
+for (var i = 0; i < 1000000; i++) {
+  stackList.push(new Stack());
+  queueList.push(new Queue());
 }
