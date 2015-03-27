8c4ca682e3086c50904f0b97008014030a90b04d
irfanbaqui <irfan.baqui@gmail.com>
2015-03-27 11:44:39 -0700

    Implement insert, remove and retrieve methods for the HashTable Class.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,36 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this.retrieve(k) || [];
+  bucket.push( [k,v] );
+  this._storage.set(i,bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var result;
+  if (bucket) {
+    _.each(bucket, function (tuple) {
+      if (tuple[0] === k) {
+        result = tuple[1];
+      }
+    });
+  }
+  return result;
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if (bucket) {
+    _.each(bucket, function (tuple, i) {
+      if (tuple[0] === k) {
+        tuple[1] = null ;
+      }
+    });
+  }
 };
 
 
