44c62df1e68f7576dd60436f2f5876f345a5fdb8
irfanbaqui <irfan.baqui@gmail.com>
2015-03-27 16:29:48 -0700

    Refactor HashTable to halve in size when its capacity falls below 25%

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,7 +1,6 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-
   this._size = 0;
 };
 
@@ -12,10 +11,11 @@ HashTable.prototype.insert = function(k, v){
   this._storage.set(i,bucket);
   this._size++;
 
-  // After adding, check if capacity is at 75%
+  // After adding, check if capacity reached 75%
   // If so, rehash
   if (this._size / this._limit >= .75) {
-    this.rehash();
+    this.rehash(this._limit * 2);
+    console.log('rehashed to double');
   }
 };
 
@@ -30,24 +30,45 @@ HashTable.prototype.retrieve = function(k){
       }
     });
   }
+  if (result === undefined) return null;
   return result;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  var newBucket;
+
+  console.log('Size before remove: ' + this._size);
   if (bucket) {
     _.each(bucket, function (tuple, i) {
       if (tuple[0] === k) {
-        tuple[1] = null ;
+        tuple[1] = null;
       }
     });
   }
+
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][1]) {
+      var newTuple = [];
+      newTuple.push(bucket[i][0]);
+      newTuple.push(bucket[i][1]);
+      newBucket.push(newTuple);
+    }
+  }
+  this._storage.set(i, newBucket);
+  this._size--;
+
+  // After removing, check if capacity is less than 25%
+  // If so, rehash to halve the limit
+  if (this._size / this._limit < .25) {
+    this.rehash(this._limit / 2);
+  }
 };
 
-HashTable.prototype.rehash = function() {
+HashTable.prototype.rehash = function(limit) {
   // Double the size of this._limit
-  this._limit *= 2;
+  this._limit = limit;
   this._size = 0;
 
   // Create a new instance of LimitedArray with the new size
@@ -63,8 +84,9 @@ HashTable.prototype.rehash = function() {
     _.each(bucket, function(tuple) {
       // For each tuple, generate new index with the key
       // Insert key-value pair in new instance of LimitedArray
-      // debugger;
-      that.insert(tuple[0], tuple[1]);
+      if (tuple[1]) {
+        that.insert(tuple[0], tuple[1]);
+      }
     });
   });
 };
