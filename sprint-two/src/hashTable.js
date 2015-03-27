971d7abbd126b965de28542d1decba5680d3be44
irfanbaqui <irfan.baqui@gmail.com>
2015-03-27 15:49:16 -0700

    Refactor HashTable to double in size when its capacity reaches 75%

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,8 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -8,6 +10,13 @@ HashTable.prototype.insert = function(k, v){
   var bucket = this._storage.get(i) || [];
   bucket.push( [k,v] );
   this._storage.set(i,bucket);
+  this._size++;
+
+  // After adding, check if capacity is at 75%
+  // If so, rehash
+  if (this._size / this._limit >= .75) {
+    this.rehash();
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -22,7 +31,6 @@ HashTable.prototype.retrieve = function(k){
     });
   }
   return result;
-
 };
 
 HashTable.prototype.remove = function(k){
@@ -35,6 +43,30 @@ HashTable.prototype.remove = function(k){
       }
     });
   }
+};
+
+HashTable.prototype.rehash = function() {
+  // Double the size of this._limit
+  this._limit *= 2;
+  this._size = 0;
+
+  // Create a new instance of LimitedArray with the new size
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(this._limit);
+
+  var that = this;
+
+  // Iterate through each of the buckets in the
+  // old LimitedArray instance
+  oldStorage.each(function(bucket) {
+    // For each bucket, iterate through the tuples
+    _.each(bucket, function(tuple) {
+      // For each tuple, generate new index with the key
+      // Insert key-value pair in new instance of LimitedArray
+      // debugger;
+      that.insert(tuple[0], tuple[1]);
+    });
+  });
 };
 
 /*
